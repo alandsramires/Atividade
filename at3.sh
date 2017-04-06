758b73910d2739c0050965d3ca579a4cad6e9633
alandsramires <alansramires@outlook.com>
2017-04-06 17:18:36 -0300

    Atividade 3 para entregar

--- at3.sh
+++ at3.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 clear
 echo "Você selecionou o exercicio 3"
+
 sleep 1
 sal=1
 while (( $sal != 0 )); do
   echo -n "Informe o salario a ser calculado: "
   read sal
-	if (( $sal >= 1 || $sal <= 500 )); then
-	let sal=($sal*1,2)
-	  if (( $sal > 500 )); then
-	  let sal=($sal*1,1)
-	 
-	  
-	  fi
-	fi
-  echo Salario atual: $sal
+	num=$sal
+		if (( $num <= 500)); then
+			echo -n "Salário com reajuste de 20%: R$ "
+			echo "scale=2; $num+$num*20/100" | bc
+			echo
+				else
+	   				 if (( $num > 500 )); then
+	       					echo -n "Salário com reajuste de 10%: R$ "
+					        echo "scale=2; $num+$num*10/100" | bc
+						echo	
+	   				 fi
+		fi
+
 done
